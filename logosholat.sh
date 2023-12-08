e1eacb20add880a9d2777c95e039cb9769418b80
Themolax <153324281+Themolax@users.noreply.github.com>
2023-12-09 03:41:26 +0700

    Add files via upload

---
+++ logosholat.sh
@@ -0,0 +1,40 @@
+merah="$(printf '\033[31m')"  
+ijo="$(printf '\033[32m')"  
+oren="$(printf '\033[33m')"  
+biru="$(printf '\033[34m')"
+magenta="$(printf '\033[35m')"  
+cyan="$(printf '\033[36m')"  
+putih="$(printf '\033[37m')" 
+hitam="$(printf '\033[30m')"
+bgmerah="$(printf '\033[41m')"  
+bgijo="$(printf '\033[42m')"  
+bgoren="$(printf '\033[43m')"  
+bgbiru="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  
+CYANBG="$(printf '\033[46m')"  
+WHITEBG="$(printf '\033[47m')" 
+bghitam="$(printf '\033[40m')"
+df_fg="$(printf '\033[39m')"  
+df_bg="$(printf '\033[49m')"
+
+echo "
+          ________________  ___ __ ______________
+             7  77  _  77    \ 7  V  V  77  _  77  7
+          ___|  ||  _  ||  7  ||  |  |  ||  _  ||  |
+          7  !  ||  7  ||  |  ||  !  !  ||  7  ||  !___
+          |     ||  |  ||  !  ||        ||  |  ||     7
+          !_____!!__!__!!_____!!________!!__!__!!_____!
+
+           _________________________   _______________
+           7     77  7  77     77  7   7  _  77      7
+           |  ___!|  !  ||  7  ||  |   |  _  |!__  __!
+           !__   7|     ||  |  ||  !___|  7  |  7  7
+           7     ||  7  ||  !  ||     7|  |  |  |  |
+           !_____!!__!__!!_____!!_____!!__!__!  !__! " | lolcat -p 2.3 -a -d 3 -S 2 -s 25
+echo "           ${biru}[${merah}*${biru}]${ijo}Author  $putih :$magenta The Molax &  
+           ${biru}[${merah}*${biru}]${ijo}Code by $putih :
+           ${biru}[${merah}*${biru}]${ijo}Language$putih :$magenta Python
+           ${biru}[${merah}*${biru}]${ijo}Make On $putih :$magenta 1 December 2023
+           ${biru}[${merah}*${biru}]${ijo}Theme by $putih:$magenta The Molax
+"
+
